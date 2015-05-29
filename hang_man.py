689ad5e441ab7b76c5a574146f88cc880bcfcbfb
Bekk Blando <bekkblando@gmail.com>
2015-05-29 15:04:37 -0400

    This Passes Requirments

--- hang_man.py
+++ hang_man.py
@@ -11,36 +11,35 @@ def game(text):
     computer_rand = random.choice(clean_text)
     underscore = len(computer_rand) * "_"
     underscore = list(underscore)
+    print("Number of Letters " + str(len(computer_rand)))
     print("Welcome to Jurassic Park")
     wrong = False
     while True:
         if wrong is True:
             lives -= 1
             if lives <= 0:
-                print("You Lose")
+                print("You Lose The Word is" + computer_rand)
                 break
         print("Guess's Left: " + str(lives))
-        Guess = input("Guess a letter")
-        print(computer_rand)
+        Guess = input("Guess a letter ")
+        #print(computer_rand)
+        wrong = True
         for x in computer_rand:
             if x == Guess:
                 print("YOU GUESSED IT!")
                 print("YOUS RIGHT!")
                 wrong = False
-                print(computer_rand.find(Guess))
+                # print(computer_rand.find(Guess))
                 for e, i in enumerate(computer_rand):
                     if Guess == i:
                         underscore[e] = i
                         print(underscore)
                         print(''.join(underscore))
                         if '_' not in underscore:
-                            print('you win')
-                            break
-            else:
-                wrong = True
+                            return("You Win")
                 # print("Guess's Left '" + lives)
                 # print("Not there")
 
 
 
-game(text)
+print(game(text))
