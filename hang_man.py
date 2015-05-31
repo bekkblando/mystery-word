498258b2607c8f6df638e9267957be4f3085d6ba
Bekk Blando <bekkblando@gmail.com>
2015-05-30 22:36:53 -0400

    Hard Mode
    Hard Mode Seems done

--- hang_man.py
+++ hang_man.py
@@ -7,44 +7,46 @@ def text(file_to_use):  # brings in file
         return text
 
 
-def get_word(text_to_clean, diff):  # Cleans the text
+def get_word(text_to_clean):  # Cleans the text
     print("loading")
     clean_text = text_to_clean.lower().split('\n')  # lower text, put in list
     computer_rand = random.choice(clean_text)  # pick random word
-    if difficuluty(computer_rand, diff) == True:
-        underscore = len(computer_rand) * "_"  # find the length of text
-        underscore = list(underscore)  # make the underscore string "____"
-        return ((underscore, computer_rand))  # Send the value out in a tuple
-    elif difficuluty(computer_rand, diff) == False:
-        pass
+    diff = get_diff()
+    while True:
+        if difficuluty(computer_rand, diff) == True:
+            underscore = len(computer_rand) * "_"  # find the length of text
+            underscore = list(underscore)  # make the underscore string "____"
+            return (underscore, computer_rand)  # Send the value out in a tuple
+        elif difficuluty(computer_rand, diff) == False:
+            computer_rand = random.choice(clean_text)
 
 # For some reason I dont like tuples
 
 
 def difficuluty(computer_rand, diff):
     # nothing yet
-    if get.diff.lower() == "easy":
+    if diff == "easy":
         if len(computer_rand) >= 4 and len(computer_rand) <= 6:
             return True
         else:
-            pass
+            return False
             # word = get_word((text('/usr/share/dict/words')), "easy")
-    if get.diff.lower() == "normal":
+    if diff == "normal":
         if len(computer_rand) >= 6 and len(computer_rand) <= 10:
             return True
         else:
-            pass
+            return False
             # word = get_word((text('/usr/share/dict/words')), "normal")
-
-    if get.diff.lower() == "hard":
+    if diff == "hard":
         if len(computer_rand) >= 10:
             return True
         else:
-            pass
+            return False
             # word = get_word((text('/usr/share/dict/words')), "hard")
 
 
 def game(tuple):  # Main game
+    # print(tuple)
     tried_words = []
     underscore = tuple[0]  # Unpack the tuple
     computer_rand = tuple[1]  # Unpack the tuple
@@ -55,34 +57,47 @@ def game(tuple):  # Main game
         if checked is True:  # deducte lives if checked is True
             lives -= 1
             if lives <= 0:  # IF lives is less then 0 or equal to you're done
+                print("Guess's Left " + str(lives))
                 print("You Lose The Word is" + computer_rand)
                 break
         if checked == "You Win":  # If checked equals win you win
             print("You Win")  # print you win, Do I really need to explain this
             exit()
-        print("Guess's Left: " + str(lives))  # Lives left
         Guess = input("Guess a letter ")  # Player Guess
         if is_guess_allowed(Guess, tried_words) == True:
             checked = check_guess(Guess, computer_rand, underscore)  # Run chek
+            print("Guess's Left: " + str(lives))  # Lives left
         elif is_guess_allowed(Guess, tried_words) == False:
+            print("Guess's Left: " + str(lives))  # Lives left
             checked = False
 
 """This will check that the user puts in just 1 letter"""
 
 
-def is_guess_allowed(Guess, tried_words):
-    print(not False)
-    if Guess.isalpha is not False:
-        print("Not a letter")
+def is_guess_allowed(Guess, tried_words):  # Guess checking
+    # if Guess.isalpha == 0:
+        # print("Not a letter")
+        # return False
+    try:
+        if Guess.isalpha() == False:
+            print("Not a letter!")
+            return False
+        int(Guess)
+        float(Guess)
+        print("No Numbers")
         return False
+    except:
+        pass
     if len(Guess) != 1:
         print("Not that many")
         return False
     if Guess not in tried_words:
+        print(chr(27) + "[2J")
         tried_words.append(Guess)
         print(tried_words)
         return True
     elif Guess in tried_words:
+        print(chr(27) + "[2J")
         print("Already Guessed")
         print(tried_words)
         return False
@@ -95,8 +110,8 @@ if the guess is wrong it will return a True if not it will return False"""
 
 
 def check_guess(Guess, computer_rand, underscore):
-    # print(computer_rand.find(Guess))
     if computer_rand.find(Guess) == -1:  # Check if the letters not in the word
+        print(' '.join(underscore))
         return(True)  # Return true for wrong no need to keep going
     for x in computer_rand:  # Iterate over the word and find letter
         if x == Guess:  # You guessed it
@@ -114,7 +129,7 @@ def check_guess(Guess, computer_rand, underscore):
 try:  # error checking
     assert isinstance(text('/usr/share/dict/words'), str) == True
     print("Text funciton works")
-    assert type(get_word(text('/usr/share/dict/words'))) == tuple
+    # assert isinstance(get_word(text('/usr/share/dict/words')), tuple) == True
     print("get_word function works")
     assert check_guess('a', 'anna', list('____')) == False
     assert check_guess('b', 'anna', list('____')) == True
@@ -129,7 +144,7 @@ def start_game():  # starts the game
         start = input("Want to play? y/n ")  # gets input to start game
         start = start.lower()  # lower case words
         if start == 'y':
-            get_word()
+            game(get_word(text('/usr/share/dict/words')))
         if start == 'n':
             break
         else:
@@ -146,5 +161,6 @@ def get_diff():
         return 'hard'
     else:
         print("Thats not a difficuluty try again")
+        game(get_word(text('/usr/share/dict/words')))
 
 start_game()
